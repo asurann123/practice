4c4299869ff7311cf2e1d06486c5d87472f3dcc5
SotaNaoi <>
2020-07-16 15:02:13 +0900

    クリックして値を変更するのはできた

--- js/mouse.js
+++ js/mouse.js
@@ -1,3 +1,23 @@
 $(function(){
   console.log('読み込み完了');
+
+  $(".msg").each(function() {
+    $(this).data("info",["こんにちは","こんばんは","さようなら"]);
+    $(this).data("count",-1);
+  });
+
+  $(".msg").click(function(event) {
+    var n = $(this).data("count");
+    var arr = $(this).data("info");
+    n = (n + 1) % (arr.length);
+    $(this).text(arr[n]);
+    $(this).data("count",n);
+  });
+
+  $(".msg").hover(function() {
+    $(this).addClass('hovermsg');
+  },function(event){
+    $(this).removeClass("hovermsg");
+  }
+);
 });
