7dbf09497bdae9b1ec1dc67b8aea88e55c4df191
SotaNaoi <>
2020-07-16 14:19:50 +0900

    jsの勉強をする

---
+++ datasample.php
@@ -0,0 +1,7 @@
+<?php 
+
+$date = date("Y-m-d ");
+$current_start_date = $date . "10:00:00";
+$current_end_date = $date . "14:00:00";
+var_dump($current_end_date);
+?>
---
+++ js/sample.js
@@ -0,0 +1,24 @@
+
+$(function() {
+  console.log('DOM読み込み完了');
+});
+
+$("button").on("click", function(){
+
+  $(this).fadeOut('slow', function() {
+    $(this).text("ふぇぇぇぇぇ");
+  });
+
+  $("h1").fadeOut('slow', function() {
+    $(this).text("さよなら");
+  });
+
+  //$('#egg').fadeOut('slow');
+
+  $("#hiyoko").fadeIn(2000);
+
+  $("#message").fadeIn(2000);
+
+
+
+});
