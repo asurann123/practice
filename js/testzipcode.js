cfa6ed3d0c68cbd38c2777c4179a9d6a9dcb3412
SotaNaoi <>
2020-07-31 12:03:11 +0900

    よくわからんけど追加

---
+++ js/testzipcode.js
@@ -0,0 +1,27 @@
+$(window).ready( function() {
+  console.log('DOM読み込み完了');
+
+    //自動で郵便番号を入力する
+    $('#zip').jpostal({
+      postcode : [
+        '#zip'
+      ],
+      address : {
+        '#prefecture'  : '%3',
+        '#town' : '%5',
+        '#all' : '%4',
+      }
+
+    });
+
+    //郵便番号の入力が空になった場合の処理
+    $('#zip').on('keyup', function(event){
+      if ($(this).val() == '') {
+        $('#prefecture').val('');
+        $('#city').val('');
+        $('#town').val('');
+        $('#all').val('');
+      }
+    });
+  });
+  
