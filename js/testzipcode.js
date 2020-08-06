7e15554167815a27920c0f70158c83319dfbf6bc
SotaNaoi <>
2020-08-06 11:00:12 +0900

    今日のコミット戻しはここに戻してください

--- js/testzipcode.js
+++ js/testzipcode.js
@@ -1,27 +1,19 @@
 $(window).ready( function() {
   console.log('DOM読み込み完了');
 
-    //自動で郵便番号を入力する
-    $('#zip').jpostal({
-      postcode : [
-        '#zip'
-      ],
-      address : {
-        '#prefecture'  : '%3',
-        '#town' : '%5',
-        '#all' : '%9',
-      }
-
-    });
-
     //郵便番号の入力が空になった場合の処理
-    $('#zip').on('keyup', function(event){
+    $('.p-postal-code').on('keyup', function(event){
       if ($(this).val() == '') {
         $('#prefecture').val('');
         $('#city').val('');
         $('#town').val('');
         $('#all').val('');
       }
+    });
+
+    $('#ok-btn').click(function(event) {
+      var hoge = $(".p-locality").val();
+      console.log(hoge);
     });
 
 });
